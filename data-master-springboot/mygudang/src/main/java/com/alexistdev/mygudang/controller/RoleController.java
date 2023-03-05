03c47ca68ba8dc7af04ac9b2849745ab2f93c0d7
alexistdev <alexistdev@gmail.com>
2023-03-05 22:09:27 +0700

    feat(role-entity): create role entity and add end point

---
+++ data-master-springboot/mygudang/src/main/java/com/alexistdev/mygudang/controller/RoleController.java
@@ -0,0 +1,21 @@
+package com.alexistdev.mygudang.controller;
+
+import com.alexistdev.mygudang.entity.Role;
+import com.alexistdev.mygudang.service.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/roles")
+public class RoleController {
+    @Autowired
+    private RoleService roleService;
+
+    @PostMapping
+    public Role create(@RequestBody Role role){
+        return roleService.save(role);
+    }
+}
