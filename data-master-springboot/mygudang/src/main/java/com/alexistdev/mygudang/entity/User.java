d6d77f53ad0252db7f4120595316c65a51411fd7
alexistdev <alexistdev@gmail.com>
2023-03-06 13:22:53 +0700

    feat(user-role-linkage): add relation between users and role

--- data-master-springboot/mygudang/src/main/java/com/alexistdev/mygudang/entity/User.java
+++ data-master-springboot/mygudang/src/main/java/com/alexistdev/mygudang/entity/User.java
@@ -46,6 +46,9 @@ public class User implements Serializable {
     @Temporal(TIMESTAMP)
     private Date createdDate;
 
+    @ManyToOne
+    private Role role;
+
     public User() {
     }
 
@@ -58,4 +61,67 @@ public class User implements Serializable {
         this.isActive = isActive;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(int isActive) {
+        this.isActive = isActive;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
 }
